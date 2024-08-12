2393bad80aab95bd8825d064896de8dd59179bee
JJ <yeiyeithirteen@gmail.com>
2024-08-12 18:46:29 -0400

    Initial Commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,24 @@
+vim.opt.tabstop = 2           -- Tamano de tab
+vim.opt.expandtab = true      -- Transformar tab a espacios
+vim.opt.shiftwidth = 2
+vim.g.mapleader = ' '         -- Tecla leader
+
+vim.opt.number = true         -- Shos line numbers
+vim.opt.mouse = 'a'           -- Habilita mouse a = all modes
+vim.opt.scrolloff = 5         -- Padding de lineas del borde
+vim.opt.relativenumber = true
+vim.opt.ruler = true          -- Muestra col, row del cursor
+vim.opt.fillchars = 'eob:-'   -- Character empty line
+vim.opt.ignorecase = true     -- Ignorar uppercase en busqqueda
+vim.opt.smartcase = true      -- Buscar uppercase solo si hay uppercase 
+vim.opt.hlsearch = false      -- Resaltar resultado busqueda anterior
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<CR>')
+
+vim.keymap.set({'n', 'x'}, 'gy', '"+y')
+vim.keymap.set({'n', 'x'}, 'gp', '"+p')
+
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
+vim.keymap.set({'n', 'x'}, 'X', '"_d')
