68f4d29dd6338e86d878a7ca2ebee4c9cf8cf439
JJ <yeiyeithirteen@gmail.com>
2024-08-13 15:37:45 -0400

    Modifica hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,24 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>ft', ':Neotree filesystem reveal left<CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignored = true,
+				hide_by_name = {
+					".github",
+				},
+				never_show = { ".git" },
+			},
+		},
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>ft", ":Neotree filesystem reveal left<CR>")
+	end,
 }
