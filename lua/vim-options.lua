54016e6703e7e8aab31ff5fe9f23d8e4fe14961a
JJ <yeiyeithirteen@gmail.com>
2024-08-13 15:00:39 -0400

    Cambio opcion

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,14 +3,14 @@ vim.opt.expandtab = true      -- Transformar tab a espacios
 vim.opt.shiftwidth = 2
 vim.g.mapleader = ' '         -- Tecla leader
 
-vim.opt.number = true         -- Shos line numbers
+vim.opt.number = true         -- Show line numbers
 vim.opt.mouse = 'a'           -- Habilita mouse a = all modes
-vim.opt.scrolloff = 5         -- Padding de lineas del borde
+vim.opt.scrolloff = 8         -- Padding de lineas del borde
 vim.opt.relativenumber = true
 vim.opt.ruler = true          -- Muestra col, row del cursor
 vim.opt.fillchars = 'eob:-'   -- Character empty line
 vim.opt.ignorecase = true     -- Ignorar uppercase en busqqueda
-vim.opt.smartcase = true      -- Buscar uppercase solo si hay uppercase 
+vim.opt.smartcase = true      -- Buscar uppercase solo si hay uppercase en la busqueda
 vim.opt.hlsearch = false      -- Resaltar resultado busqueda anterior
 vim.opt.wrap = true
 vim.opt.breakindent = true
